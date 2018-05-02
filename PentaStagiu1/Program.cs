43d568ebdd3c418727c0c059a7df30dbd16545c8
Mărgărit Marian Cătălin <margarit.catalin97@gmail.com>
2018-05-02 19:03:12 +0300

    Am creat jocul cerut in prima tema .

--- PentaStagiu1/Program.cs
+++ PentaStagiu1/Program.cs
@@ -10,6 +10,32 @@ namespace PentaStagiu1
     {
         static void Main(string[] args)
         {
+            GhicesteNumarul();
+
+        }
+        static void GhicesteNumarul()
+        {
+            Random rand = new Random();
+            int randomNumber = rand.Next(0, 100);
+            string choiseNumber;
+            do
+            {
+                Console.WriteLine("Introduceti numarul :");
+                choiseNumber = Console.ReadLine();
+                if (int.Parse(choiseNumber) == randomNumber)
+                {
+                    Console.WriteLine("Felicitari! Ai ghicit numarul.");
+
+                }
+                else if (int.Parse(choiseNumber) < randomNumber)
+                {
+                    Console.WriteLine($"{int.Parse(choiseNumber)} este mai mic decat cel ales random.");
+                }
+                else
+                {
+                    Console.WriteLine($"{int.Parse(choiseNumber)} este mai mare decat cel ales random.");
+                }
+            } while (int.Parse(choiseNumber) != randomNumber);
         }
     }
 }
